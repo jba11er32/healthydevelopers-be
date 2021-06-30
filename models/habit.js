9defb1944a5e015ebd411fdc41907db8346c8e18
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-30 14:17:13 -0700

    Created env.local file, created UserSchema for authentication

--- models/habit.js
+++ models/habit.js
@@ -1,17 +1,19 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
-const HabitSchema = new Schema(
-    {
-        date: { type: String, required: true },
-        water: Number,
-        pushups: Number,
-        situps: Number,
-        squats: Number,
-        owner: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user',
-            required: true,
-        }
-    }
-)
+const HabitSchema = new Schema({
+	date: { type: String, required: true },
+	water: Number,
+	pushups: Number,
+	situps: Number,
+	squats: Number,
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user',
+		required: true,
+	},
+});
+
+const Habit = mongoose.model('Habit', HabitSchema);
+
+module.exports = Habit;

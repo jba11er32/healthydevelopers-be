9965982473738cbe5b0779a9ff0537ee6495e809
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-30 13:28:56 -0700

    Schema added to habit.js in models

--- models/habit.js
+++ models/habit.js
@@ -1,2 +1,17 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
+
+const HabitSchema = new Schema(
+    {
+        date: { type: String, required: true },
+        water: Number,
+        pushups: Number,
+        situps: Number,
+        squats: Number,
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        }
+    }
+)

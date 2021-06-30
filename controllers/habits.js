5e0d608ce61c82d3cae18a158cfecf223f0a1e53
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-30 15:48:03 -0700

    Added .gitignore file, updated habits.js model, and created CRUD functions for habits controller

--- controllers/habits.js
+++ controllers/habits.js
@@ -3,8 +3,37 @@ const router = express.Router();
 
 const Habit = require('../models/habit');
 
-router.get('/', (req, res, next) => {
-    Habit.find({})
-        .then((habits) => res.json(habits))
-        .catch(next);
+router.get('/', requireToken, (req, res, next) => {
+	Habit.find({})
+		.then((habits) => res.json(habits))
+		.catch(next);
 });
+
+router.get('/:id', requireToken, (req, res, next) => {
+	const id = req.params.id;
+	Habit.findById(id)
+		.then((habit) => res.json(habit))
+		.catch(next);
+});
+
+router.post('/', requireToken, (req, res, next) => {
+	const owner = req.user._id;
+	Habit.create({ ...req.body, owner })
+		.then((habit) => res.json(habit))
+		.catch(next);
+});
+
+router.put('/:id', requireToken, (req, res, next) => {
+	const id = req.params.id;
+	const updated = req.body;
+	Habit.find;
+});
+
+router.delete('/:id', requireToken, (req, res, next) => {
+	const id = req.params.id;
+	Habit.findByIdAndDelete(id)
+		.then(() => res.sendStatus(204))
+		.catch(next);
+});
+
+module.exports = router;
--- models/habit.js
+++ models/habit.js
@@ -1,18 +1,22 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
-const HabitSchema = new Schema({
-	date: { type: String, required: true },
-	water: Number,
-	pushups: Number,
-	situps: Number,
-	squats: Number,
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-		required: true,
+const HabitSchema = new Schema(
+	{
+		water: Number,
+		pushups: Number,
+		situps: Number,
+		squats: Number,
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
 	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const Habit = mongoose.model('Habit', HabitSchema);
 

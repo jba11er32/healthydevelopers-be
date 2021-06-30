1c1a2e279bbe45354f31bc92cd482d03e24173d7
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-30 16:46:02 -0700

    Created request_logger.js, auth.js, and custom_errors.js in middleware and added reqeustlogger and handleerrors inside of index.js

--- controllers/habits.js
+++ controllers/habits.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { requireToken } = require('../middleware/auth');
 
 const Habit = require('../models/habit');
 
--- index.js
+++ index.js
@@ -5,8 +5,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const requestLogger = require('./middleware/request_logger');
+app.use(requestLogger);
+
 const habitsControllers = require('./controllers/habits.js');
 app.use('/api/habits', habitsControllers);
+
+const { handleErrors } = require('./middleware/custom_errors');
+app.use(handleErrors);
 
 app.listen(4000, () => {
 	console.log('app listening on port 4000');

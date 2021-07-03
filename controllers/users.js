9f9205329cbc0a92c0c6fc7ae8fac6165a600528
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-07-03 16:04:02 -0700

    Take out GET function from users.js

--- controllers/users.js
+++ controllers/users.js
@@ -5,12 +5,6 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const { createUserToken } = require('../middleware/auth');
 
-router.get('/users', (req, res, next) => {
-	User.find({})
-		.then((user) => res.json(user))
-		.catch(next);
-});
-
 router.post('/signup', async (req, res, next) => {
 	try {
 		const firstName = req.body.firstName;
